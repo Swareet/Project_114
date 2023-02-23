779799788531c251e5f12097e7774850704825bd
Swareet <95135157+Swareet@users.noreply.github.com>
2023-02-23 18:04:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.position(470);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){ 
+}
+
+function snapshot()
+{
+}
